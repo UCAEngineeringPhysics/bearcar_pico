6e35b6a1feef393c69bcda3662a9087bdfaaca18
linzhanguca <lzhang12@uca.edu>
2026-05-22 17:00:43 -0500

    add IMU class

---
+++ upython_scripts/perception/inertial_sensor.py
@@ -0,0 +1,121 @@
+from machine import Pin, I2C
+from utime import sleep
+from math import pi
+
+
+class MPU6050:
+    def __init__(self, i2c_channel=0, scl_id=9, sda_id=8, i2c_addr=0x68, dlpf_level=4):
+        self.board_led = Pin(25, Pin.OUT)
+        self.i2c = I2C(i2c_channel, scl=Pin(scl_id), sda=Pin(sda_id), freq=400_000)
+        self.i2c_addr = i2c_addr
+        self.i2c.writeto_mem(
+            self.i2c_addr,
+            0x6B,  # PWR_MGMT_1 register address
+            bytes([0x00]),  # data
+        )  # wake up sensor
+        assert 0 <= dlpf_level <= 6
+        self.i2c.writeto_mem(
+            self.i2c_addr, 0x1A, bytes([dlpf_level])
+        )  # filter out high freq noise, level 3 ~= 42 Hz
+        # Variables
+        self.lin_acc_x = 0.0
+        self.lin_acc_y = 0.0
+        self.lin_acc_z = 0.0
+        self.ang_vel_x = 0.0
+        self.ang_vel_y = 0.0
+        self.ang_vel_z = 0.0
+        # Constant
+        self.gyro_bias_x = 0.0
+        self.gyro_bias_y = 0.0
+        self.gyro_bias_z = 0.0
+        sleep(1)  # hands off
+        self.calibrate_gyro()
+
+    def read_data(self):
+        """
+        MPU6050 uses 2 bytes to represent values of an entity.
+        Acc_X, Acc_Y, Acc_Z, Temp, Gyro_X, Gyro_Y, Gyro_Z are stored in contiguous registers.
+        To read'em all, grab 14 bytes starting at the ACCEL_XOUT_H register: 0x3B.
+        """
+
+        def process_raw(data, scale):
+            """
+            Args:
+                data: a list contains n words(2 bytes) of sensor data
+                scale: a constant coefficient scaling raw data.
+                       accelerometer: 16384 per g
+                       gyro: 131 per deg/s, TODO: use radians
+            Returns:
+                value: human readible value in m/s^2 or deg/s
+            """
+            if data > 32768:
+                value = (data - 65535) / scale
+            else:
+                value = data / scale
+
+            return value
+
+        words = self.i2c.readfrom_mem(
+            self.i2c_addr,
+            0x3B,  # ACCEL_XOUT_H register address
+            14,  # number of bytes
+        )  # retrieve raw sensor data in bytes
+        # Preprocess bytes, split 2 bytes as a group
+        data = [words[i] << 8 | words[i + 1] for i in range(0, len(words), 2)]
+        # Calculate human readibles
+        self.lin_acc_x = process_raw(data[0], 16384) * 9.80665
+        self.lin_acc_y = process_raw(data[1], 16384) * 9.80665
+        self.lin_acc_z = process_raw(data[2], 16384) * 9.80665
+        self.ang_vel_x = process_raw(data[4], 131) * pi / 180 - self.gyro_bias_x
+        self.ang_vel_y = process_raw(data[5], 131) * pi / 180 - self.gyro_bias_y
+        self.ang_vel_z = process_raw(data[6], 131) * pi / 180 - self.gyro_bias_z
+
+        return {
+            "lin_acc_x": self.lin_acc_x,
+            "lin_acc_y": self.lin_acc_y,
+            "lin_acc_z": self.lin_acc_z,
+            "ang_vel_x": self.ang_vel_x,
+            "ang_vel_y": self.ang_vel_y,
+            "ang_vel_z": self.ang_vel_z,
+        }
+
+    def calibrate_gyro(self, num_samples=1000):
+        """
+        One-time calibration
+        """
+        omg_x_deposite = 0.0
+        omg_y_deposite = 0.0
+        omg_z_deposite = 0.0
+        # print("Calibrating Gyro... DO NOT MOVE ROBOT")  # debug
+        for i in range(num_samples):
+            data = self.read_data()
+            omg_x_deposite += data["ang_vel_x"]
+            omg_y_deposite += data["ang_vel_y"]
+            omg_z_deposite += data["ang_vel_z"]
+            sleep(0.005)  # 5ms delay to let the sensor refresh
+            if i % 50 == 0:  # debug
+                self.board_led.toggle()
+        self.gyro_bias_x = omg_x_deposite / num_samples
+        self.gyro_bias_y = omg_y_deposite / num_samples
+        self.gyro_bias_z = omg_z_deposite / num_samples
+        # print(f"Calibration Done. Gyro biases x={self.gyro_bias_x}, y={self.gyro_bias_y}, z={self.gyro_bias_z}")  # debug
+
+
+if __name__ == "__main__":
+    from utime import ticks_ms, sleep_ms
+
+    # SETUP
+    sensor = MPU6050(i2c_channel=1, scl_id=15, sda_id=14, i2c_addr=0x68)
+
+    # LOOP
+    while True:
+        data = sensor.read_data()
+        # Logging, enable plotter
+        print(f"[Pico time stamp: {ticks_ms()} ms]:")
+        print(
+            f"acc(m/s/s): x={data['lin_acc_x']:.4f}, y={data['lin_acc_y']:.4f}, z={data['lin_acc_z']:.4f}"
+        )
+        print(
+            f"angv(rad/s): x={data['ang_vel_x']:.4f}, y={data['ang_vel_y']:.4f}, z={data['ang_vel_z']:.4f}"
+        )
+        sleep_ms(100)  # 10Hz
