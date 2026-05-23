544fe5979598f4f59adafdc60f6f5ac05e21de0c
linzhanguca <lzhang12@uca.edu>
2026-05-23 15:29:27 -0500

    remove acc filter

--- upython_scripts/pico_interface.py
+++ upython_scripts/pico_interface.py
@@ -22,13 +22,9 @@ imu = MPU6050(i2c_channel=1, scl_id=15, sda_id=14, i2c_addr=0x68)
 messenger = select.poll()
 messenger.register(sys.stdin, select.POLLIN)
 # Constants
-tx_period_us = 16_667 # 60 Hz
-smooth_coeff = 0.85  # low-pass filter coefficient for accel
-deadzone_acc = 0.05  # m/s^2 (Ignore micro-vibrations)
+tx_period_us = 16_667  # 60 Hz
 # Variables
-filtered_acc_x = 0.0  # init accel
-lin_vel_x = 0.0
-mode = 's'  # standby
+mode = "s"  # standby
 # print("Pico is ready...")  # debug
 
 # LOOP
@@ -37,15 +33,9 @@ while True:
     # Transmit data (TX)
     now_us = ticks_us()
     if ticks_diff(now_us, last_us) >= tx_period_us:
-        # Extract linear velocity on x and angular velocity on z
+        # Extract angular velocity on z
         motion_data = imu.read_data()
-        filtered_acc_x = (smooth_coeff * filtered_acc_x) + ((1 - smooth_coeff) * motion_data['lin_acc_x'])
-        if abs(filtered_acc_x) < deadzone_acc:
-            filtered_acc_x = 0.0
-            lin_vel_x *= 0.05 
-            if abs(lin_vel_x) < 0.01: v_x = 0.0
-        lin_vel_x += filtered_acc_x * tx_period_us * 1e-6
-        out_msg = f"{lin_vel_x:.3f},{motion_data['ang_vel_z']:.3f}"
+        out_msg = f"{motion_data['ang_vel_z']:.3f}"
         print(out_msg)  # main.py will send this to computer
         last_us = now_us  # update last time stamp
     # Receive data (RX)
@@ -55,10 +45,11 @@ while True:
         if len(in_msg) == 3:
             try:
                 rgb_led.glow(in_msg[0])
-                drivetrain.set_angle(int(in_msg[1]))
-                drivetrain.set_speed(int(in_msg[2]))
+                driver.set_angle(int(in_msg[1]))
+                driver.set_speed(int(in_msg[2]))
             except ValueError:
-                pass
+                rgb_led.glow("e")
+                driver.stop()
         else:
-            rgb_led.glow('e')
-            drivetrain.stop()
+            rgb_led.glow("e")
+            driver.stop()
