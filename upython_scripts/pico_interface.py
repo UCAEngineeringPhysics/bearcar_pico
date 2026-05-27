dbeacfd1b2ed2ca35a38cf0a75b0c443475155d0
linzhanguca <lzhang12@uca.edu>
2026-05-26 22:30:51 -0500

    switch back to standby if communication closed

--- upython_scripts/pico_interface.py
+++ upython_scripts/pico_interface.py
@@ -16,6 +16,7 @@ from perception.inertial_sensor import MPU6050
 freq(240_000_000)  # Pico 2 original: 150_000_000
 # Instantiate components on the board
 rgb_led = Illuminator(red_pin_id=21, green_pin_id=20, blue_pin_id=19)
+rgb_led.glow("s")  # standby
 driver = Drivetrain(steering_pin_id=16, throttle_pin_id=17)
 imu = MPU6050(i2c_channel=1, scl_id=15, sda_id=14, i2c_addr=0x68)
 # Config USB messenger
@@ -23,27 +24,27 @@ messenger = select.poll()
 messenger.register(sys.stdin, select.POLLIN)
 # Constants
 tx_period_us = 16_667  # 60 Hz
-# Variables
-mode = "s"  # standby
-rgb_led.glow(mode)
+rx_timeout_us = 500_000  # 0.5 seconds
 # print("Pico is ready...")  # debug
 
 # LOOP
-last_us = ticks_us()
+last_tx_us = ticks_us()
+last_rx_us = ticks_us()
 try:
     while True:
         # Transmit data (TX)
         now_us = ticks_us()
-        if ticks_diff(now_us, last_us) >= tx_period_us:
+        if ticks_diff(now_us, last_tx_us) >= tx_period_us:
             # Extract angular velocity on z
             motion_data = imu.read_data()
             out_msg = f"{motion_data['ang_vel_z']:.3f}"
             print(out_msg)  # main.py will send this to computer
-            last_us = now_us  # update last time stamp
+            last_tx_us = now_us  # update last time stamp
         # Receive data (RX)
         is_waiting = messenger.poll(0)  # check data in USB
         if is_waiting:
             in_msg = sys.stdin.readline().strip().split(",")
+            last_rx_us = now_us
             if len(in_msg) == 3:
                 try:
                     rgb_led.glow(in_msg[0])
@@ -51,17 +52,20 @@ try:
                         driver.set_angle(int(in_msg[1]))
                         driver.set_speed(int(in_msg[2]))
                     else:
-                        driver.stop()                    
+                        driver.stop()
                 except ValueError:
                     rgb_led.glow("e")
                     driver.stop()
             else:
                 rgb_led.glow("e")
                 driver.stop()
+        # Switch mode to STANDBY if waited too long
+        if ticks_diff(now_us, last_rx_us) >= rx_timeout_us:
+            rgb_led.glow("s")
+            driver.stop()
 except Exception as e:
     # print('Pico reset')  # debug
     reset()
 finally:
     # print('Pico reset')  # debug
     reset()
-
